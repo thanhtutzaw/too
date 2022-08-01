347391f31150b2bfe0a83127d2ef0cc8d0c0e4b7
Than Htut Zaw <zgyi025@gmail.com>
2022-08-01 17:49:20 +0630

    route id conditional

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -38,25 +38,27 @@ function NewHeader() {
 // const user = useAuthUser()
 const Note = ({ note }) => {
   // const AuthUser = useAuthUser()
-  // const router = useRouter()
-  // let { id } = router.query
+  const router = useRouter()
+  let { id } = router.query
   // id = parseInt(id)
   return (
     <>
       <LayoutGroup>
         <Home />
         {/* <Header /> */}
+        {id && 
         <AnimatePresence>
-          {/* <p className={styles.viewContainer}>{id}</p> */}
-          {/* <Link scroll={false} href='/' ><a className={styles.viewContainer}>{id}</a></Link> */}
-          <Link scroll={false} href='/'>
-          <div className={styles.viewContainer}>
-            <p>{note.title}</p>
-            <p>{note.text}</p>
-          </div>
-          </Link>
+        {/* <p className={styles.viewContainer}>{id}</p> */}
+        {/* <Link scroll={false} href='/' ><a className={styles.viewContainer}>{id}</a></Link> */}
+        <Link scroll={false} href='/'>
+        <div className={styles.viewContainer}>
+          <p>{note.title}</p>
+          <p>{note.text}</p>
+        </div>
+        </Link>
 
-        </AnimatePresence>
+      </AnimatePresence>
+        }
       </LayoutGroup>
     </>
 
