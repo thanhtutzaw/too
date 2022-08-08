76630856f1669d9d3242303f60e58be12fb742b8
Than Htut Zaw <zgyi025@gmail.com>
2022-08-08 17:20:53 +0630

    vh chrome test

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -42,24 +42,35 @@ const Note = ({ note }) => {
   const router = useRouter()
   let { id } = router.query
   // id = parseInt(id)
+  let height
+  if (typeof window !== "undefined") {
+     height = window.innerHeight
+    //  height =730 - 673
+    if(height > 673){
+      height = 57 +" extra px need (full screen)"
+    }
+  }
   return (
     <>
       {/* <Header /> */}
       {id ?
-        <> <div className={styles.viewContainer}>
+        <> 
+
+        <div className={styles.viewContainer}>
           
             <div onClick={()=>window.history.back()} className={styles.backBtn}>
               <BiArrowBack />
             </div>
-          
-          <motion.div className={styles.titleView} layoutId={`title-${id}`} contentEditable="true" aria-multiline="true" role="textbox" tabIndex="0" aria-label="Title" spellCheck="true" >
+          <p>Height {height}</p>
+          {/* <motion.div className={styles.titleView} layoutId={`title-${id}`} contentEditable="true" aria-multiline="true" role="textbox" tabIndex="0" aria-label="Title" spellCheck="true" >
             {note.title}
           </motion.div>
           <motion.div className={styles.textView} layoutId={`title-${id}`} contentEditable="true" aria-multiline="true" role="textbox" tabIndex="0" aria-label="Title" spellCheck="true" >
             {note.text}
-          </motion.div>
+          </motion.div> */}
 
         </div>
+
         </>
         :
         null
