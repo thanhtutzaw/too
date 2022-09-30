5216c3fc0bdde29cc8545bf2249a55e0d475e067
ThanHtutZaw <zgyi5025@gmail.com>
2022-09-30 12:22:05 +0630

    fixed: responsive

--- Components/Header.js
+++ Components/Header.js
@@ -5,11 +5,12 @@ import Link from "next/link";
 import Sidebar from "./Sidebar";
 import { motion } from 'framer-motion'
 import { CgClose } from 'react-icons/cg'
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuthUser, withAuthUser, withAuthUserTokenSSR } from "next-firebase-auth";
 // import { useAuthContext } from '../../context/UserAuthState';
 
 function Header({ user }) {
+    const input = useRef(null)
     const [showModal, setshowModal] = useState(false);
 
 
@@ -17,8 +18,10 @@ function Header({ user }) {
     const [Search, setSearch] = useState();
     // const {user} = useAuthContext()
     const [isClose, setisClose] = useState(false);
-    const searchCloseHandle = () => {
+
+    const searchCloseHandle = (e) => {
         setSearch('')
+        input.current.focus()
         setisClose(true)
     }
     const modalHandle = () => {
@@ -57,8 +60,16 @@ function Header({ user }) {
                 <Link href='/'>
                     <h1 className={styles.logo}>Too</h1>
                 </Link>
+                {/* {user.photoURL ?
+                    // <>{user.displayName}</>
+                    <motion.div className={styles.mainProfile} onClick={modalHandle} whileTap={{ scale: .8 }}>
+                        <Image referrerPolicy="no-referrer" unoptimized={true} src={user?.photoURL} alt="test" width="40" height="40" className={styles.profile}></Image>
+                    </motion.div>
+                    :
+                    <><Link href="/auth"><a className={styles.signinBtn}>Sign in</a></Link></>
+                } */}
                 <div className={styles.searchBar}>
-                    <input value={Search} onChange={(e) => setSearch(e.target.value)} className={styles.searchInput} disabled={user ? '' : 'disable'} type="text" />
+                    <input ref={input} value={Search} onChange={(e) => setSearch(e.target.value)} className={styles.searchInput} disabled={user ? '' : 'disable'} type="text" placeholder="Search"/>
                     <CgClose onClick={searchCloseHandle} className={Search ? styles.searchCloseBtn : styles.searchCloseBtnHide} />
                 </div>
 
