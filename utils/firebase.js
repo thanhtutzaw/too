ffbd8128a8c760430187e1e97ac8c311c9f49022
ThanHtutZaw <zgyi5025@gmail.com>
2022-11-17 10:06:59 +0630

    modify : firebasejs env

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,25 +1,16 @@
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getApps, initializeApp } from "firebase/app";
+
 
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAqiYmXkm_fDhvVICxEF_JPcNvn1zKQpPs",
-    authDomain: "too-dc752.firebaseapp.com",
-    projectId: "too-dc752",
-    storageBucket: "too-dc752.appspot.com",
-    messagingSenderId: "190703670735",
-    appId: "1:190703670735:web:ee69e434991f2c5304aeba",
-    measurementId: "G-HLWGM0RSC2"
-    // apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-    // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    // // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    // // messagingSenderId: process.env.REACT_APP_MESSAGING_SEDNER_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
 const apps = getApps()
-    
+
 let app
 if (!apps.length) {
     app = initializeApp(firebaseConfig);
