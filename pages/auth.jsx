f976e3dde020f02d95c6c85a5375b422d799f185
thanhtutzaw <zgyi5025@gmail.com>
2023-05-01 15:12:40 +0630

    logo animate

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -29,7 +29,12 @@ const Auth = () => {
   return (
     <>
       <div className={styles.loginWrapper}>
-        <Image width={150} height={150} alt="logo" src={"/logo.svg"} />
+        <div className={styles.logo}>
+          <Image width={150} height={150} alt="logo" src={"/logo.svg"} />
+        </div>
+        {/* <div>
+          <Image width={150} height={150} alt="logo" src={"/logo.svg"} />
+        </div> */}
         <h1>
           Welcome to <Link href="https://github.com/thanhtutzaw/too">Too</Link>
         </h1>
